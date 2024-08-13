4a1921d2208bb01d48e5ad1c945a3902b6d7f171
Alexander Kita <Alexander.Kita@ibm.com>
2024-08-13 16:52:14 -0500

    Fix log message

--- ibm/acctest/acctest.go
+++ ibm/acctest/acctest.go
@@ -1047,7 +1047,7 @@ func init() {
 	Pi_image_bucket_region = os.Getenv("PI_IMAGE_BUCKET_REGION")
 	if Pi_image_bucket_region == "" {
 		Pi_image_bucket_region = "us-east"
-		fmt.Println("[INFO] Set the environment variable PI_IMAGE_BUCKET_REGION for testing ibm_pi_image_export resource else it is set to default value 'us-east'")
+		fmt.Println("[INFO] Set the environment variable PI_IMAGE_BUCKET_REGION for testing ibm_pi_image resource else it is set to default value 'us-east'")
 	}
 
 	Pi_image_id = os.Getenv("PI_IMAGE_ID")
