3cdfda4f8e911d94539fca6d226be275a3a0471a
Alexander Kita <Alexander.Kita@ibm.com>
2024-08-19 17:23:03 -0500

    test fixes

--- ibm/service/power/resource_ibm_pi_capture_test.go
+++ ibm/service/power/resource_ibm_pi_capture_test.go
@@ -179,11 +179,11 @@ func testAccCheckIBMPICaptureConfigBasic(name string) string {
 func testAccCheckIBMPICaptureCloudStorageConfig(name string) string {
 	return fmt.Sprintf(`
 	resource "ibm_pi_capture" "capture_instance" {
-		pi_cloud_instance_id="%[1]s"
+		pi_cloud_instance_id="%s"
 		pi_capture_name  = "%s"
 		pi_instance_name = "%s"
 		pi_capture_destination = "cloud-storage"
-		pi_capture_cloud_storage_region = "us-east"
+		pi_capture_cloud_storage_region = "%s"
 		pi_capture_cloud_storage_access_key = "%s"
 		pi_capture_cloud_storage_secret_key = "%s"
 		pi_capture_storage_image_path = "%s"
@@ -194,7 +194,7 @@ func testAccCheckIBMPICaptureCloudStorageConfig(name string) string {
 func testAccCheckIBMPICaptureBothConfig(name string) string {
 	return fmt.Sprintf(`
 	resource "ibm_pi_capture" "capture_instance" {
-		pi_cloud_instance_id="%[1]s"
+		pi_cloud_instance_id="%s"
 		pi_capture_name = "%s"
 		pi_instance_name = "%s"
 		pi_capture_destination  = "both"
