491decb50fb9eea524c8b8b702e154dbdbf5844d
Alexander Kita <Alexander.Kita@ibm.com>
2024-10-17 15:17:01 -0500

    Add test for vsn resource

--- ibm/acctest/acctest.go
+++ ibm/acctest/acctest.go
@@ -228,7 +228,7 @@ var (
 	Pi_snapshot_id                  string
 	Pi_spp_placement_group_id       string
 	Pi_target_storage_tier          string
-	Pi_virtual_serial_number_id     string
+	Pi_virtual_serial_number        string
 	Pi_volume_clone_task_id         string
 	Pi_volume_group_id              string
 	Pi_volume_group_name            string
@@ -1222,10 +1222,10 @@ func init() {
 		fmt.Println("[INFO] Set the environment variable PI_VOLUME_CLONE_TASK_ID for testing Pi_volume_clone_task_id resource else it is set to default value 'terraform-test-volume-clone-task-id'")
 	}
 
-	Pi_virtual_serial_number_id = os.Getenv("PI_VIRTUAL_SERIAL_NUMBER_ID")
+	Pi_virtual_serial_number = os.Getenv("PI_VIRTUAL_SERIAL_NUMBER")
 	if Pi_volume_clone_task_id == "" {
 		Pi_volume_clone_task_id = "terraform_test_power"
-		fmt.Println("[INFO] Set the environment variable PI_VIRTUAL_SERIAL_NUMBER_ID for testing ibm_pi_virtual_serial_number data source else it is set to default value 'terraform-test-power'")
+		fmt.Println("[INFO] Set the environment variable PI_VIRTUAL_SERIAL_NUMBER for testing ibm_pi_virtual_serial_number data source else it is set to default value 'terraform-test-power'")
 	}
 
 	Pi_resource_group_id = os.Getenv("PI_RESOURCE_GROUP_ID")
