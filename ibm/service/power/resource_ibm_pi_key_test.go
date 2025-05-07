d0644e3f3c769ae9b6383224bcc52c3699be2d53
Alexander Kita <Alexander.Kita@ibm.com>
2025-05-07 15:38:23 -0500

    test changes

--- ibm/service/power/resource_ibm_pi_key_test.go
+++ ibm/service/power/resource_ibm_pi_key_test.go
@@ -20,6 +20,7 @@ import (
 )
 
 func TestAccIBMPIKey_basic(t *testing.T) {
+	keyRes := "ibm_pi_key.key"
 	publicKey := strings.TrimSpace(`
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
 `)
@@ -32,9 +33,12 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVE
 			{
 				Config: testAccCheckIBMPIKeyConfig(publicKey, name),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckIBMPIKeyExists("ibm_pi_key.key"),
-					resource.TestCheckResourceAttr(
-						"ibm_pi_key.key", "pi_key_name", name),
+					testAccCheckIBMPIKeyExists(keyRes),
+					resource.TestCheckResourceAttr(keyRes, "pi_key_name", name),
+					resource.TestCheckResourceAttr(keyRes, "primary_workspace", "true"),
+					resource.TestCheckResourceAttrSet(keyRes, "creation_date"),
+					resource.TestCheckResourceAttr(keyRes, "key", publicKey),
+					resource.TestCheckResourceAttr(keyRes, "name", name),
 				),
 			},
 		},
